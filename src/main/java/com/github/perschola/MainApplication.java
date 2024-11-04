5cc8ff250f354eb994e0ba920eb6b04d30f8de8b
Mohammed N <mnaser11218@gmail.com>
2024-11-03 20:27:33 -0500

    added console class and implemented add and delete methods

--- src/main/java/com/github/perschola/MainApplication.java
+++ src/main/java/com/github/perschola/MainApplication.java
@@ -3,6 +3,10 @@ package com.github.perschola;
 public class MainApplication {
     public static void main(String[] args) {
         Runnable myObject = new MyObject();
-        myObject.run();
+        Console console = new Console();
+        console.run();
+//        myObject.run();
+        console.addRow("19", "mohammed", "3", "4");
+
     }
 }
